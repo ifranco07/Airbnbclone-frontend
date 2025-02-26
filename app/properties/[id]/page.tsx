37f38fa2e0eced303e6f0fe60c447efe12c3bd7c
ifranco07 <i.francomtz07@gmail.com>
2025-02-26 11:55:49 -0600

    05 Detail page for properties (static)

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,54 @@
+import ReservationSidebar from "@/app/components/properties/ReservationSiderbar";
+import Image from "next/image";
+
+
+const PropertyDetailPage = () => {
+    return (
+        <main className="max-w-[1500px] mx-auto px-6 pb-6">
+            <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+                <Image
+                    fill 
+                    src='/beach_1.jpg'
+                    className="object-cover w-full h-full"
+                    alt="Beach house"
+                />
+            </div>
+
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+                <div className="py-6 pr-6 col-span-3">
+                    <h1 className="mb-4 text-4xl">Property name</h1>
+
+                    <span className="mb-6 block text-lg text-gray-600">
+                        4 guests · 2 bedrooms · 1 bathroom
+                    </span>
+
+                    <hr />
+
+                    <div className="py-6 flex items-center space-x-4">
+                        <Image
+                            src='/profile_pic_1.jpg'
+                            width={50}
+                            height={50}
+                            className="rounded-full"
+                            alt="User name"
+                        />
+                        
+                        <p><strong>John doe</strong> is your host</p>
+                    </div>
+
+                    <hr />
+
+                    <p className="mt-6 text-lg">
+                    OVERTIGO SOMBRA LADRILLO DRONES JAZZ PLUMA KERNEL FANTASMA BOSQUE
+                     NEXUS GIRASOL VULCANO ASTRO ROJO PIXEL SIRENA
+                    </p>
+                </div>
+                <div>
+                    <ReservationSidebar / >
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default PropertyDetailPage;
