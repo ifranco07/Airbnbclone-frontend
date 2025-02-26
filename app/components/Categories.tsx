4fc81d80e875914f1ac8490cdc1a7885d58160d9
ifranco07 <i.francomtz07@gmail.com>
2025-02-25 21:05:07 -0600

    04 List properties on the front page (static)

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -5,7 +5,7 @@ const Categories = () => {
     <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
         <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
             <Image 
-                src="/icn_category_beach.jpg" 
+                src="/icn_category_beach.jpeg" 
                 alt="Category - Beach" 
                 width={20} 
                 height={20} 
@@ -15,7 +15,7 @@ const Categories = () => {
 
         <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
             <Image 
-                src="/icn_category_beach.jpg" 
+                src="/icn_category_beach.jpeg" 
                 alt="Category - Beach" 
                 width={20} 
                 height={20} 
@@ -25,7 +25,7 @@ const Categories = () => {
 
         <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
             <Image 
-                src="/icn_category_beach.jpg" 
+                src="/icn_category_beach.jpeg" 
                 alt="Category - Beach" 
                 width={20} 
                 height={20} 
@@ -35,7 +35,7 @@ const Categories = () => {
 
         <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
             <Image 
-                src="/icn_category_beach.jpg" 
+                src="/icn_category_beach.jpeg" 
                 alt="Category - Beach" 
                 width={20} 
                 height={20} 
