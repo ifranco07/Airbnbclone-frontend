557dea588727815f2058d38830ac071c7db34227
ifranco07 <i.francomtz07@gmail.com>
2025-02-27 10:50:01 -0600

    12 Create reusable modal window

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -5,11 +5,13 @@ import{useState} from "react";
 import MenuLink from "./MenuLink";
 
 const UserNav = () => {
-    const [isOpen, setIsOpen] = useState(true)
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className="p-2 relative inline-block border rounded-full">
-            <button className="flex items-center" > <span className="sr-only">Menú de usuario</span>
+            <button
+                onClick={()=>setIsOpen(!isOpen)}
+                className="flex items-center" > <span className="sr-only">Menú de usuario</span>
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+      <p>Yo yo</p>
+    )
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        <div className="pt-32">
-          
+        <div className="pt-32"> 
         {children}
         </div>
       </body>
