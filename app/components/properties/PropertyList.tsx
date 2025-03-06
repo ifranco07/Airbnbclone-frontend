f4821d19f0c02d536934b778b41daff44677eeb2
ifranco07 <i.francomtz07@gmail.com>
2025-03-06 09:53:46 -0600

    16 Create api services file (And use itin the properties list)

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -2,33 +2,22 @@
 
 import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
+import apiService from "@/app/services/apiService";
 
 export type PropertyType = {
     id: string;
     tittle: string;
     image_url: string;
     price_per_night: number;
-
 }
 
 const PropertyList = () => {
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
     const getProperties= async () => {
-        const url = 'http://localhost:8000/api/properties/';
+         const tmpProperties = await apiService.get('/api/properties/')
 
-        await fetch(url, {
-            method: 'GET',
-        })
-            .then(response => response.json())
-            .then((json) => {
-                console.log('json', json);
-
-                setProperties(json.data)
-            })
-            .catch((error) => {
-                console.error('error', error);
-            });
+        setProperties(tmpProperties.data);
     };
 
     useEffect(() => {
