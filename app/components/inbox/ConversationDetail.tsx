362509266354143fcf182b776f74caf2e8c4eb2b
ifranco07 <i.francomtz07@gmail.com>
2025-03-26 22:31:08 -0600

    31 Store and load messages

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -11,12 +11,14 @@ interface ConversationDetailProps {
     token: string;
     userId: string;
     conversation: ConversationType;
+    messages: MessageType [];
 }
 
 
 const ConversationDetail: React.FC<ConversationDetailProps> = ({
     userId,
     token,
+    messages,
     conversation
 }) => {
     const messagesDiv = useRef(null);
@@ -25,7 +27,7 @@ const ConversationDetail: React.FC<ConversationDetailProps> = ({
     const otherUser = conversation.users?.find((user) => user.id != userId);
     const [realtimeMessages, setRealTimeMessages]= useState<MessageType []>([]);
 
-    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`${process.env.NEXT_PUBLIC_WS_HOST}/ws/${conversation.id}/?token=${token}/`, {
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`${process.env.NEXT_PUBLIC_WS_HOST}/ws://127.0.0.1:8000/api/chat/${conversation.id}/?token=${token}/`, {
         share: false,
         shouldReconnect: () => true,
       },
@@ -84,6 +86,16 @@ const ConversationDetail: React.FC<ConversationDetailProps> = ({
                 ref={messagesDiv}
                 className="max-h-[400px] overflow-auto flex flex-col space-y-4"
             >
+                {messages.map((message, index) => (
+                    <div
+                        key={index}
+                        className={`w-[80%] px-6 rounded-xl ${message.created_by.name == myUser?.name ? 'ml-[20%] bg-blue-200' : 'bg-gray-200'}`}
+                    >
+                        <p className="font-bold text-gray-500">{message.created_by.name}</p>
+                        <p>{message.body}</p>
+                    </div>
+                ))}
+
                 {realtimeMessages.map((message, index) => (
                     <div
                         key={index}
